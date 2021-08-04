3d94de8ec203aafa1b061a3176020cb1b03aa8c9
EvaRoets <eva_roets@hotmail.com>
2021-08-04 10:46:57 +0200

    add default in function construct

--- exercise_1_classes.php
+++ exercise_1_classes.php
@@ -6,14 +6,13 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-
 class Beverages
 {
     public string $color;
     public float $price;
-    public string $temperature = cold;
+    public string $temperature;
 
-    public function __construct(string $color, float $price, string $temperature)
+    public function __construct(string $color, float $price, string $temperature = "cold")
     {
         $this->temperature = $temperature;
 
@@ -27,11 +26,7 @@ class Beverages
 
 $cola = new Beverages("black", 2, "$this.temperature");
 
-var_dump($cola);
+getInfo($cola);
 
-/* EXERCISE 1
- the price equals 2 euro and the temperature to cold automatically
- print the getInfo on the screen.
-print the temperature on the screen.
-USE TYPE HINTING EVERYWHERE!
-*/
+
+
